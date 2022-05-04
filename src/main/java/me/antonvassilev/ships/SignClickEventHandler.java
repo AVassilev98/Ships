b8a1b79263efa33a1d0c494fc3b65a4493d51da0
Evan Symmes <evan@symmes.to>
2022-05-03 22:47:33 -0700

    Refactor sign click handling, consolidate data classes (#3)
    * Add steering signs, clean-up some code
    
    * Add steering signs, clean-up some code
    
    * mad?
    
    * Make steering left & right click based
    
    Also clean up some code
    
    * Fix sign click handler
    
    * Typo

--- src/main/java/me/antonvassilev/ships/SignClickEventHandler.java
+++ src/main/java/me/antonvassilev/ships/SignClickEventHandler.java
@@ -1,11 +1,14 @@
 package me.antonvassilev.ships;
 
+import com.google.common.collect.ImmutableList;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.Plugin;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public class SignClickEventHandler implements Listener {
@@ -13,56 +16,94 @@ public class SignClickEventHandler implements Listener {
     private final Plugin owningPlugin;
     private final HashMap<String, Vessel> vessels;
 
+    private static final List<String> CLICKABLE_SIGNS_BY_METADATA_VALUES =
+            ImmutableList.of(
+                    Vessel.EngineSign.METADATA_VALUE, Vessel.SteeringSign.METADATA_VALUE);
+
     public SignClickEventHandler(Plugin owningPlugin, HashMap<String, Vessel> vessels) {
         this.owningPlugin = owningPlugin;
         this.vessels = vessels;
     }
 
+    /**
+     * Handles player right click events on signs. If the sign belongs to a ship,
+     * dispatch the event to the handlers.
+     * @param event PlayerInteractEvent
+     */
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
-        switch (event.getAction())
-        {
+        if (event.getAction().isRightClick()) {
+            // Make sure the block we clicked is a ship sign.
+            Optional<String> signType = ShipUtils.getMetadataStringFromBlock(
+                    Vessel.VESSEL_CONTROL_TYPE_METADATA_KEY,
+                    Objects.requireNonNull(event.getClickedBlock()), owningPlugin);
+            if(!signType.isPresent() ||
+                    !CLICKABLE_SIGNS_BY_METADATA_VALUES.contains(signType.get())) {
+                return;
+            }
+            owningPlugin.getLogger().info(signType.get());
+            // Find the vessel by the name extracted from engine sign metadata and
+            // then handle the type of sign.
+            ShipUtils.getMetadataStringFromBlock(Vessel.VESSEL_NAME_METADATA_KEY,
+                    event.getClickedBlock(), owningPlugin)
+                    .map(vessels::get)
+                    .ifPresent(vessel -> handleShipSign(event, signType.get(), vessel));
+        }
+    }
+
+    private void handleLicenseSign(Vessel vessel, PlayerInteractEvent event) {
+        // Do nothing
+    }
+
+    private void handleSteeringSign(Vessel vessel, PlayerInteractEvent event) {
+        switch (event.getAction()) {
+            case LEFT_CLICK_BLOCK: {
+                vessel.rotateRight();
+                break;
+            }
             case RIGHT_CLICK_BLOCK: {
-                // Make sure the block we clicked is a vessel engine.
-                Optional<String> controlType = ShipUtils.getMetadataStringFromBlock(
-                        Vessel.VESSEL_CONTROL_TYPE_METADATA_KEY, event.getClickedBlock(), owningPlugin);
-                if (!controlType.isPresent() ||
-                        !controlType.get().equals(Vessel.EngineSign.METADATA_KEY)) {
-                    return;
-                }
-                // Find the vessel by the name extracted from engine sign metadata and
-                // move the vessel forward if found.
-                ShipUtils.getMetadataStringFromBlock(Vessel.VESSEL_NAME_METADATA_KEY,
-                                event.getClickedBlock(), owningPlugin)
-                        .map(vessels::get)
-                        .ifPresent(Vessel::moveForward);
+                vessel.rotateLeft();
+            }
+            default:
+                break;
+        }
+    }
+
+    private void handleEngineSign(Vessel vessel, PlayerInteractEvent event) {
+        switch (event.getAction()) {
+            case RIGHT_CLICK_BLOCK: {
+                vessel.moveForward();
                 break;
             }
             case LEFT_CLICK_BLOCK: {
-                Optional<String> controlType = ShipUtils.getMetadataStringFromBlock(
-                        Vessel.VESSEL_CONTROL_TYPE_METADATA_KEY, event.getClickedBlock(), owningPlugin);
-                if (!controlType.isPresent() ||
-                        !controlType.get().equals(Vessel.EngineSign.METADATA_KEY)) {
-                    return;
-                }
-
-                Optional<Vessel> owningVessel = ShipUtils.getMetadataStringFromBlock(Vessel.VESSEL_NAME_METADATA_KEY,
-                                event.getClickedBlock(), owningPlugin)
-                        .map(vessels::get);
-                if (!owningVessel.isPresent())
-                {
-                    break;
-                }
-
                 if (event.getPlayer().isSneaking())
-                    owningVessel.get().decrementVelocity();
+                    vessel.decrementVelocity();
                 else
-                    owningVessel.get().incrementVelocity();
-
+                    vessel.incrementVelocity();
                 break;
             }
             default:
                 break;
         }
+    }
+
+    private void handleShipSign(PlayerInteractEvent event, String signType, Vessel vessel) {
+        Optional<Vessel.ShipSignType> type = Vessel.ShipSignType.metadataStringToShipSignType(signType);
+        type.ifPresent(shipSignType -> {
+            switch (shipSignType) {
+                case LICENSE:
+                    handleLicenseSign(vessel, event);
+                    break;
+                case STEERING:
+                    handleSteeringSign(vessel, event);
+                    break;
+                case ENGINE:
+                    handleEngineSign(vessel, event);
+                    break;
+                case UNKNOWN:
+                default:
+                    owningPlugin.getLogger().info("Ship control sign with unknown type clicked");
+            }
+        });
     }
 }
--- src/main/java/me/antonvassilev/ships/Vessel.java
+++ src/main/java/me/antonvassilev/ships/Vessel.java
@@ -1,7 +1,6 @@
 package me.antonvassilev.ships;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.TorchBlock;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -23,60 +22,9 @@ import java.util.HashSet;
 import java.util.*;
 import static java.lang.Math.min;
 import static java.lang.Math.max;
-
+import java.util.stream.Collectors;
 
 public class Vessel {
-
-    private class BlockInfo
-    {
-        private CraftBlockState state;
-        private final int priority;
-        public BlockInfo(CraftBlockState state)
-        {
-            this.state = state;
-            BlockData blockData = state.getBlockData();
-            Material block = state.getType();
-            if (blockData instanceof Sign ||
-                blockData instanceof Switch ||
-                blockData instanceof Rail ||
-                blockData instanceof RedstoneWire ||
-                blockData instanceof Ageable ||
-                block == Material.TORCH ||
-                block == Material.WALL_TORCH ||
-                block == Material.REDSTONE_TORCH ||
-                block == Material.REDSTONE_WALL_TORCH ||
-                block == Material.SOUL_TORCH ||
-                block == Material.SOUL_WALL_TORCH
-            ) {
-                Bukkit.getLogger().info("Found Attachable");
-                this.priority = 1;
-            }
-            else {
-                this.priority = 0;
-            }
-        }
-
-        public int getPriority()
-        {
-            return this.priority;
-        }
-        public CraftBlockState getState()
-        {
-            return this.state;
-        }
-        public int getX()
-        {
-            return this.state.getX();
-        }
-        public int getY()
-        {
-            return this.state.getY();
-        }
-        public int getZ()
-        {
-            return this.state.getZ();
-        }
-    };
 
     final private static int MAX_VESSEL_SZ = 5000;
     // size of workList -> max number of blocks to check for largest case (single column of blocks)
@@ -91,69 +39,70 @@ public class Vessel {
     private final String name;
     private final ArrayList<BlockInfo> m_blocks = new ArrayList<BlockInfo>();
     private final LicenseSign licenseSign;
+    private SteeringSign steeringSign;
     private EngineSign engineSign;
     private int xBlockOffset = 0;
     private int yBlockOffset = 0;
     private int zBlockOffset = 0;
 
-    Vessel(Plugin owningPlugin, String name, Block startBlock)
-    {
+    Vessel(Plugin owningPlugin, String name, Block startBlock) {
         this.name = name;
         this.owningPlugin = owningPlugin;
         this.world = startBlock.getWorld();
 
         // Engine sign isn't necessary to create vessel, can be added after.
         this.engineSign = null;
+        this.steeringSign = null;
 
         // Set the metadata for the licenseSign block, then discover vessel blocks.
         startBlock.setMetadata(VESSEL_NAME_METADATA_KEY,
                 new FixedMetadataValue(owningPlugin, name));
         startBlock.setMetadata(VESSEL_CONTROL_TYPE_METADATA_KEY,
-                new FixedMetadataValue(owningPlugin, LicenseSign.METADATA_KEY));
-        this.licenseSign = new LicenseSign(startBlock.getState());
-        this.xBlockOffset = this.licenseSign.block.getX();
-        this.yBlockOffset = this.licenseSign.block.getY();
-        this.zBlockOffset = this.licenseSign.block.getZ();
+                new FixedMetadataValue(owningPlugin, LicenseSign.METADATA_VALUE));
 
+        this.licenseSign = new LicenseSign(startBlock.getState());
+        this.xBlockOffset = this.licenseSign.state.getX();
+        this.yBlockOffset = this.licenseSign.state.getY();
+        this.zBlockOffset = this.licenseSign.state.getZ();
         discoverVesselFromBlock(startBlock);
     }
 
     /**
      * Sets the metadata on an engine sign block and adds it to the vessel.
-     * @param eventBlock Block
+     * @param block Block
      */
-    public void addEngineSign(Block eventBlock) {
-        eventBlock.setMetadata(VESSEL_CONTROL_TYPE_METADATA_KEY,
-                new FixedMetadataValue(owningPlugin, EngineSign.METADATA_KEY));
-        eventBlock.setMetadata(VESSEL_NAME_METADATA_KEY,
+    public void addEngineSign(Block block) {
+        if(this.engineSign != null) {
+            owningPlugin.getLogger().info("Engine already registered for ship.");
+            return;
+        }
+        block.setMetadata(VESSEL_CONTROL_TYPE_METADATA_KEY,
+                new FixedMetadataValue(owningPlugin, EngineSign.METADATA_VALUE));
+        block.setMetadata(VESSEL_NAME_METADATA_KEY,
                 new FixedMetadataValue(owningPlugin, name));
-        CraftBlockState blockState = (CraftBlockState) eventBlock.getState();
+        CraftBlockState blockState = (CraftBlockState) block.getState();
         this.engineSign = new EngineSign(blockState);
         this.m_blocks.add(new BlockInfo(blockState));
     }
 
-    public void moveEngineMetadata(int x, int y, int z)
-    {
-        this.engineSign.block.getBlock().removeMetadata(
-                VESSEL_CONTROL_TYPE_METADATA_KEY, owningPlugin
-        );
-        this.engineSign.block.getBlock().removeMetadata(
-                VESSEL_NAME_METADATA_KEY, owningPlugin
-        );
+    public void moveEngineMetadata(int x, int y, int z) {
+        this.engineSign.state.getBlock().removeMetadata(
+                VESSEL_CONTROL_TYPE_METADATA_KEY, owningPlugin);
+        this.engineSign.state.getBlock().removeMetadata(
+                VESSEL_NAME_METADATA_KEY, owningPlugin);
 
-        int newX = this.engineSign.block.getX() + x;
-        int newY = this.engineSign.block.getY() + y;
-        int newZ = this.engineSign.block.getZ() + z;
+        int newX = this.engineSign.state.getX() + x;
+        int newY = this.engineSign.state.getY() + y;
+        int newZ = this.engineSign.state.getZ() + z;
 
         Block newEngineBlock = this.world.getBlockAt(newX, newY, newZ);
         newEngineBlock.setMetadata(VESSEL_CONTROL_TYPE_METADATA_KEY,
-                new FixedMetadataValue(owningPlugin, EngineSign.METADATA_KEY));
+                new FixedMetadataValue(owningPlugin, EngineSign.METADATA_VALUE));
         newEngineBlock.setMetadata(VESSEL_NAME_METADATA_KEY,
                 new FixedMetadataValue(owningPlugin, name));
     }
 
-    void setStatePosition(CraftBlockState block, int x, int y, int z)
-    {
+    void setStatePosition(CraftBlockState block, int x, int y, int z) {
         Field positionField = null;
         try {
             positionField = CraftBlockState.class.getDeclaredField("position");
@@ -167,8 +116,11 @@ public class Vessel {
         }
     }
 
-    private void discoverVesselFromBlock(Block start_block)
-    {
+    public void addSteeringSign(Block block, String direction) {
+        this.steeringSign = new SteeringSign(block.getState());
+    }
+
+    private void discoverVesselFromBlock(Block start_block) {
         HashSet<Block> visitedBlocks = new HashSet<>();
         ArrayDeque<Block> workList = new ArrayDeque<>(MAX_SEARCH_SPACE);
         workList.add(start_block);
@@ -177,8 +129,8 @@ public class Vessel {
         {
             Block curBlock = workList.remove();
             if (curBlock.isLiquid() ||
-                curBlock.isEmpty() ||
-                visitedBlocks.contains(curBlock)
+                    curBlock.isEmpty() ||
+                    visitedBlocks.contains(curBlock)
             )
             {
                 continue;
@@ -221,10 +173,7 @@ public class Vessel {
     }
 
     public void moveForward() {
-        // TODO: Forward is not always +x direction.
-
-        switch (engineSign.getMovementDirection())
-        {
+        switch (engineSign.getMovementDirection()) {
             case EAST:
             case EAST_NORTH_EAST:
             case EAST_SOUTH_EAST:
@@ -266,6 +215,7 @@ public class Vessel {
     public void rotateRight() {
         // TODO: Implement
     }
+
     public void rotateLeft() {
         // TODO: Implement
     }
@@ -295,31 +245,31 @@ public class Vessel {
     // Containers for the different types of signs
     //
     static class ShipSign {
-        protected final BlockState block;
-        public ShipSign(BlockState block) {
-            this.block = block;
+        protected final BlockState state;
+        public ShipSign(BlockState state) {
+            this.state = state;
         }
     }
 
     static class EngineSign extends ShipSign {
         int velocity;
         private static final int MAX_VELOCITY = 10;
-        public static final String METADATA_KEY = "ENGINE";
-        public EngineSign(BlockState block) {
-            super(block);
+        public static final String METADATA_VALUE = "ENGINE";
+        public EngineSign(BlockState state) {
+            super(state);
             this.velocity = 1;
         }
-        public void incrementVelocity()
-        {
+
+        public void incrementVelocity() {
             this.velocity = min(this.velocity + 1, MAX_VELOCITY);
         }
-        public void decrementVelocity()
-        {
+
+        public void decrementVelocity() {
             this.velocity = max(this.velocity + 1, 1);
         }
-        public BlockFace getMovementDirection()
-        {
-            BlockData signData = block.getBlockData();
+
+        public BlockFace getMovementDirection() {
+            BlockData signData = state.getBlockData();
             if (signData instanceof Rotatable) {
                 return ((Rotatable) signData).getRotation();
             }
@@ -328,9 +278,107 @@ public class Vessel {
     }
 
     static class LicenseSign extends ShipSign {
-        public static final String METADATA_KEY = "LICENSE";
-        public LicenseSign(BlockState block) {
-            super(block);
+        public static final String METADATA_VALUE = "LICENSE";
+        public LicenseSign(BlockState state) {
+            super(state);
+        }
+    }
+
+    static class SteeringSign extends ShipSign {
+        public static final String METADATA_VALUE = "STEERING";
+        public SteeringSign(BlockState state) {
+            super(state);
+        }
+    }
+
+    private static class BlockInfo {
+        private final CraftBlockState state;
+        private final int priority;
+        public BlockInfo(CraftBlockState state) {
+            this.state = state;
+            BlockData blockData = state.getBlockData();
+            Material block = state.getType();
+            if (blockData instanceof Sign ||
+                    blockData instanceof Switch ||
+                    blockData instanceof Rail ||
+                    blockData instanceof RedstoneWire ||
+                    blockData instanceof Ageable ||
+                    block == Material.TORCH ||
+                    block == Material.WALL_TORCH ||
+                    block == Material.REDSTONE_TORCH ||
+                    block == Material.REDSTONE_WALL_TORCH ||
+                    block == Material.SOUL_TORCH ||
+                    block == Material.SOUL_WALL_TORCH
+            ) {
+                Bukkit.getLogger().info("Found Attachable");
+                this.priority = 1;
+            }
+            else {
+                this.priority = 0;
+            }
+        }
+
+        public CraftBlockState getState() { return this.state; }
+        public int getPriority() { return this.priority; }
+        public int getX() { return this.state.getX(); }
+        public int getY() { return this.state.getY(); }
+        public int getZ() { return this.state.getZ(); }
+    };
+
+    /**
+     * Enum class that supports string initialization, and allows fetching enum
+     * by string value.
+     */
+    enum ShipSignType {
+        LICENSE("[name]", "LICENSE"),
+        STEERING("[steer]", "STEERING"),
+        ENGINE("[move]", "ENGINE"),
+        UNKNOWN("unknown", "UNKNOWN");
+
+        private static final Map<String, ShipSignType> strToShipSignTypeMap =
+                Arrays.stream(ShipSignType.values()).collect(Collectors.toMap(
+                        ShipSignType::getValue,
+                        ShipSignType::getShipSignType
+                ));
+
+        private static final Map<String, ShipSignType> metadataValueToSignTypeMap =
+                Arrays.stream(ShipSignType.values()).collect(Collectors.toMap(
+                        ShipSignType::getMetadataValue,
+                        ShipSignType::getShipSignType
+                ));
+
+        private static final Map<ShipSignType, String> shipSignTypeToStringMap =
+                Arrays.stream(ShipSignType.values()).collect(Collectors.toMap(
+                        ShipSignType::getShipSignType,
+                        ShipSignType::getValue
+                ));
+
+        private final ShipSignType shipSignType;
+        private final String value;
+        private final String metadataValue;
+
+        ShipSignType(String str, String metadataValue) {
+            this.shipSignType = this;
+            this.value = str;
+            this.metadataValue = metadataValue;
+        }
+
+        public ShipSignType getShipSignType() { return shipSignType; }
+        public String getValue() { return value; }
+        public String getMetadataValue() { return metadataValue; }
+
+        public static ShipSignType shipSignTypeFromString(String str) {
+            ShipSignType type = strToShipSignTypeMap.get(str);
+            if (type != null) return type;
+            return UNKNOWN;
+        }
+
+        public static Optional<String> strToShipSignType(ShipSignType signType) {
+            return Optional.ofNullable(shipSignTypeToStringMap.get(signType));
+        }
+
+        public static Optional<ShipSignType> metadataStringToShipSignType(String metadataValue) {
+            return Optional.ofNullable(metadataValueToSignTypeMap.get(metadataValue));
         }
     }
 }
